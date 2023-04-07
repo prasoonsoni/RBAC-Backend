187d2fe6c4cacda5d86b3b3fcabe27955e56e476
Prasoon Soni <prasoonsoni03@gmail.com>
2023-04-08 00:10:52 +0530

    Added getAllRoles function

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -1,6 +1,16 @@
 import Role from "../models/Role"
 import { Request, Response } from "express"
 
+const getAllRoles = async (req: Request, res: Response) => {
+    try {
+        const roles = await Role.find({})
+        return res.json({ success: true, message: "Message Roles Found Successfully", data: roles })
+    } catch (error: any) {
+        console.log(error.message)
+        return res.json({ success: false, message: "Internal Server Error Occurred" })
+    }
+}
+
 const createRole = async (req: Request, res: Response) => {
     try {
         const { name, description, permissions } = req.body
@@ -15,4 +25,6 @@ const createRole = async (req: Request, res: Response) => {
     }
 }
 
-export default { createRole }
+
+
+export default { getAllRoles, createRole }
--- src/routes/roleRoutes.ts
+++ src/routes/roleRoutes.ts
@@ -3,6 +3,7 @@ import roleController from "../controllers/roleController"
 
 const router: Router = express.Router()
 
+router.get('/get/all', roleController.getAllRoles)
 router.post('/create', roleController.createRole)
 
 export default router
